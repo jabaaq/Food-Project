0f04bfe026acf36f4a7e85b2de32fb0f8739282c
Jaba <jabaqadagishvili4@gmail.com>
2023-07-22 18:22:50 +0400

    updated Webpack

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 /* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 window.addEventListener('DOMContentLoaded', () => {
-
+    //webpack
     const tabs = require('./modules/tabs'),
         modal = require('./modules/modal'),
         timer = require('./modules/timer'),
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,17 @@
 'use strict';
 
 let path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './js/script.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    plugins: [new HtmlWebpackPlugin()],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
+        path: __dirname + '/js'
     },
     watch: true,
 
     devtool: "source-map",
+
+    module: {}
 };
